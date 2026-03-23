bdd79a398638f597ec400810523be85ec24a2e3c
bribzz <briacleclercq@gmail.com>
2026-03-23 14:12:34 +0000

    Feat : Test MLFlow

--- src/titanic/training/steps/load_data.py
+++ src/titanic/training/steps/load_data.py
@@ -1,5 +1,12 @@
 import logging
-# TODO : Intégrer les imports manquants
+import os
+from pathlib import Path
+import tempfile # Nouvel import pour gérer les fichiers temporaires
+
+import boto3
+import mlflow # Nouvel import pour mlflow
+import pandas as pd
+from ydata_profiling import ProfileReport
 
 
 ARTIFACT_PATH = "path_output"
@@ -7,10 +14,27 @@ PROFILING_PATH = "profiling_reports"
 
 
 def load_data(path: str) -> str:
-    logging.warning(f"load_data on path : {path}")
+  logging.warning(f"load_data on path : {path}")
 
-    # TODO : Intégrer le corps de cette fonction développée dans le notebook
+  with tempfile.TemporaryDirectory() as tmp_dir: # Utilisation d'un dossier temporaire
+    local_path = Path(tmp_dir, "data.csv") # Fichier temporaire pour stocker les données
+    logging.warning(f"to path : {local_path}")
 
-    # TODO : Dans un second temps, ajouter les logs mlflow, notamment les artifacts du profiling
-    # Mais aussi logger l'artifact du fichier csv.
+    s3_client = boto3.client(
+      "s3",
+      endpoint_url=os.environ.get("MLFLOW_S3_ENDPOINT_URL"),
+      aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+      aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"),
+    )
 
+    s3_client.download_file("kto-titanic", path, local_path)
+    df = pd.read_csv(local_path)
+
+    profile = ProfileReport(df, title=f"Profiling Report - {local_path.stem}")
+    with tempfile.NamedTemporaryFile(suffix=".html", delete=False) as tmp_file: # Fichier temporaire pour le rapport de profiling
+      profile.to_file(tmp_file.name)
+      mlflow.log_artifact(tmp_file.name, PROFILING_PATH) # Log du rapport de profiling dans mlflow
+
+    mlflow.log_artifact(str(local_path), ARTIFACT_PATH) # Log du fichier de données dans mlflow
+
+  return f"{ARTIFACT_PATH}/{local_path.name}" # Retourne le chemin dans mlflow
--- src/titanic/training/steps/split_train_test.py
+++ src/titanic/training/steps/split_train_test.py
@@ -1,7 +1,12 @@
 import logging
-# TODO : Intégrer les imports manquants
+from pathlib import Path
+import tempfile # Nouvel import pour gérer les fichiers temporaires
 
-# TODO : Dans une second temps, récupérer le client mlflow nous permettant de télécharger les artifacts enregistrés à l'étape précédente
+import mlflow # Nouvel import pour mlflow
+import pandas as pd
+import sklearn.model_selection
+
+client = mlflow.MlflowClient() # Client mlflow pour interagir avec le server de tracking
 
 FEATURES = ["Pclass", "Sex", "SibSp", "Parch"]
 
@@ -10,8 +15,26 @@ TARGET = "Survived"
 
 def split_train_test(data_path: str) -> tuple[str, str, str, str]:
     logging.warning(f"split on {data_path}")
+    # Téléchargement des données brutes depuis mlflow
+    df = pd.read_csv(client.download_artifacts(run_id=mlflow.active_run().info.run_id, path=data_path), index_col=False) 
 
-    # TODO : Implémenter le corps de la fonction avec les éléments du notebook
-    # TODO : Dans un second temps, télécharger les artifacts depuis mlflow
-    # TODO : Dans un second temps, ajouter les logs mlflow pour enregistrer les artifacts utiles pour la suite
+    y = df[TARGET]
+    x = df[FEATURES]
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.3, random_state=42)
 
+    datasets = [
+      (x_train, "xtrain", "xtrain.csv"),
+      (x_test, "xtest", "xtest.csv"),
+      (y_train, "ytrain", "ytrain.csv"),
+      (y_test, "ytest", "ytest.csv"),
+    ]
+
+    artifact_paths = []
+    with tempfile.TemporaryDirectory() as tmp_dir: # Utilisation d'un dossier temporaire
+        for data, artifact_path, filename in datasets:
+            file_path = Path(tmp_dir, filename)
+            data.to_csv(file_path, index=False)
+            mlflow.log_artifact(str(file_path), artifact_path) # Log du fichier de split dans mlflow
+            artifact_paths.append(f"{artifact_path}/{filename}") # Stockage du chemin dans mlflow
+
+    return tuple(artifact_paths)
